9c60d90c384a2d57fa1884690e9cb4c4dd42c8dd
BreezeCat <jkjk010695@gmail.com>
2020-03-20 16:36:09 +0800

    sample path

--- src/path_pub.py
+++ src/path_pub.py
@@ -15,25 +15,33 @@ path_pub = rospy.Publisher("robot_path",Path,queue_size=10)
 
 Px = []
 Py = []
+temp_Px = []
+temp_Py = []
 
 def Path_Publish():
     Robot_Path = Path()
     Robot_Path.header.frame_id = "map"
     for i in range(len(Px)):
         pose = PoseStamped()
-        pose.pose.position.x = Px[i]
-        pose.pose.position.y = Py[i]
+        pose.pose.position.x = Px[-i-1]
+        pose.pose.position.y = Py[-i-1]
         Robot_Path.poses.append(pose)
     path_pub.publish(Robot_Path)
     return
     
+def Pose_sample():
+    global Px, Py
+    Px.append(temp_Px[-1])
+    Py.append(temp_Py[-1])
+    Path_Publish()
+    return
+
 
 
 def Pose_CB(data):
-    global Px, Py
-    Px.append(data.linear.x)
-    Py.append(data.linear.y)
-    Path_Publish()
+    global temp_Px, temp_Py
+    temp_Px.append(data.linear.x)
+    temp_Py.append(data.linear.y)
     return
 
 
@@ -41,7 +49,8 @@ def Pose_CB(data):
 
 if __name__ == '__main__':
     rospy.init_node('robot_path_pub',anonymous=True)
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(10)
     sub = rospy.Subscriber("/robot_pose",Twist,Pose_CB)
     while not rospy.is_shutdown():
+        Pose_sample()
         rate.sleep()
