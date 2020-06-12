d01d798d284e1e12c538af524f6796c3cb418c0a
TX2 <jkjk010695@gmail.com>
2020-06-12 12:37:46 +0800

    add goal publisher

--- src/Stop_vel_cmd_sim.py
+++ src/Stop_vel_cmd_sim.py
@@ -7,7 +7,7 @@ import time
 
 robot1_cmd_vel = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=10)
 robot2_cmd_vel = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=10)
-
+Goal_pub = rospy.Publisher("/robot_goal", Twist, queue_size=10)
 
 
 # Constants
@@ -19,6 +19,11 @@ Vel_cmd = Twist()
 Vel_cmd.linear.x = Vcmd
 Vel_cmd.angular.z = Wcmd
 
+Goal = Twist()
+Goal.linear.x = 4.0
+Goal.linear.y = -2.5
+Goal.angular.z = 0
+
 
 def Stop_cmd(robot):
     start_time = time.time()
@@ -28,6 +33,11 @@ def Stop_cmd(robot):
     print('Release')
     return
 
+def Publish_goal():
+    Goal_pub.publish(Goal)
+    print('Publish Goal!')
+    return
+
 if __name__ == '__main__':
     rospy.init_node('stop_vel_cmd', anonymous= True)
     rate = rospy.Rate(1000)
@@ -35,6 +45,7 @@ if __name__ == '__main__':
     print("# Command list:     #")
     print("# Stop robot1 : 'A' #")
     print("# Stop robot2 : 'B' #")
+    print("# Publish Goal : 'G'#")
     print("# Stop node : 'S'   #")
     print("#####################")
     while Turn_flag:
@@ -47,9 +58,13 @@ if __name__ == '__main__':
 		print("Stop robot2 " + str(stop_time) + " s")
 		t = threading.Thread(target=Stop_cmd, args=(robot2_cmd_vel,))
 		t.start()
+	elif command == 'G':
+		t = threading.Thread(target=Publish_goal, args=())
+		t.start()
 	elif command == 'S':
 		print("Bye!")
 		Turn_flag = 0
 	else:
 		print("Command error!")
        
+
