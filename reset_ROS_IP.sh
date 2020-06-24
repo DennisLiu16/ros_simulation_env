f7859e07ff367779118d239ea86a16f9a0e71a40
TX2 <jkjk010695@gmail.com>
2020-06-24 18:16:45 +0800

    modify script for local master

--- reset_ROS_IP.sh
+++ reset_ROS_IP.sh
@@ -3,7 +3,7 @@ tag=$(cat ~/.bashrc | tail -n 1)
 if [ $tag == "#LDSCtag" ];then
     echo $tag
     echo "reset"
-    sed -i '$d' ~/.bashrc
+    #sed -i '$d' ~/.bashrc
     sed -i '$d' ~/.bashrc
     sed -i '$d' ~/.bashrc    
 else
